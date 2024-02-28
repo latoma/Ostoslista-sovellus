85dcb108c3ac00f1cd3a54ae1220f9fee7cd5fff
Toma <toma.lahtinen@helsinki.fi>
2024-02-28 19:55:55 +0200

    Fix login and registeration to database

--- schema.sql
+++ schema.sql
@@ -1,14 +1,21 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE,
-    password TEXT
+    username TEXT,
+    "password" TEXT
 );
 
--- Table for shopping lists
 CREATE TABLE shopping_lists (
     list_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
     list_name VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- Table to manage the many-to-many relationship between users and shopping lists
+CREATE TABLE user_shopping_lists (
+    user_id INT NOT NULL,
+    list_id INT NOT NULL,
+    PRIMARY KEY (user_id, list_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (list_id) REFERENCES shopping_lists(list_id)
 );
