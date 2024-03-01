be85d957bab4e2586b74492c15de35e333f09e5e
Toma <toma.lahtinen@helsinki.fi>
2024-03-01 17:19:21 +0200

    Replace previous tables with  item_lists and shared_lists tables with foreign key constraints

--- schema.sql
+++ schema.sql
@@ -1,21 +1,28 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username TEXT,
+    user_id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
     "password" TEXT
 );
 
 CREATE TABLE shopping_lists (
     list_id SERIAL PRIMARY KEY,
     list_name VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
--- Table to manage the many-to-many relationship between users and shopping lists
-CREATE TABLE user_shopping_lists (
-    user_id INT NOT NULL,
+CREATE TABLE item_lists (
+    item_id SERIAL PRIMARY KEY,
     list_id INT NOT NULL,
-    PRIMARY KEY (user_id, list_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    item_desc TEXT NOT NULL,
     FOREIGN KEY (list_id) REFERENCES shopping_lists(list_id)
+);
+
+CREATE TABLE shared_lists (
+    share_id SERIAL PRIMARY KEY,
+    shopping_list_id INT,
+    user_id INT,
+    FOREIGN KEY (shopping_list_id) REFERENCES shopping_lists(list_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    UNIQUE (shopping_list_id, user_id)
 );
