affb7a66e70ed6e27ce98a7278d2f06ce7ab6f56
Toma <toma.lahtinen@helsinki.fi>
2024-03-07 15:04:18 +0200

    fused removal functions into one function removal_mode

--- routes.py
+++ routes.py
@@ -1,41 +1,54 @@
 from app import app
 from flask import render_template, request, redirect
 import users, shopping_lists
+from datetime import datetime
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if users.user_id() == 0:
+        return render_template("index.html")
+    else:
+        return render_template("index.html", lists = users.get_shopping_lists())
 
 @app.route("/list")
 def list():
     return render_template("list.html", items = shopping_lists.get_items())
 
-@app.route("/new", methods=['POST'])
+@app.route("/new", methods=['GET', 'POST'])
 def new():
-    shopping_lists.new()
-    return redirect("/list")
+    if request.method == "GET":
+        return render_template("new.html", current_date = datetime.now().strftime('%d/%m/%Y'))
+    
+    if request.method == "POST":
+        if shopping_lists.new(request.form["list_name"]):
+            return list()
+        else:
+            return  render_template("error.html", message="Tuotteen lisääminen ei onnistunut")
 
 @app.route("/add_item", methods=["POST"])
 def add_item():
     content = request.form["content"]
     if shopping_lists.add_item(content):
-        return render_template("list.html", items = shopping_lists.get_items())
+        return list()
     else:
         return  render_template("error.html", message="Tuotteen lisääminen ei onnistunut")
     
-@app.route("/removal_mode", methods=["POST"])
+@app.route("/removal_mode", methods=["GET", "POST"])
 def removal_mode():
-    return render_template("remove.html", items = shopping_lists.get_items())
+    # Renders the "removal mode"-page
+    if request.method == "GET":
+        return render_template("remove.html", items = shopping_lists.get_items())
+    
+    # Removes selected items from shopping list
+    if request.method == "POST":
+        removed_item_ids_string = request.form.get('removedItems')
+        if removed_item_ids_string is not None and removed_item_ids_string != "":
+            # Goes through list of selected item_ids and removes them
+            removed_item_ids = [int(x) for x in removed_item_ids_string.split(",")]
+            shopping_lists.remove_items(removed_item_ids)
 
-@app.route("/confirm_removals", methods=["POST"])
-def confirm_removals():
-    # Puts the ids of removed items into a list and calls remove_items()
-    removed_item_ids_string = request.form.get('removedItems')
-    if removed_item_ids_string != "":
-        removed_item_ids = [int(x) for x in removed_item_ids_string.split(",")]
-        shopping_lists.remove_items(removed_item_ids)
+        return list()
 
-    return render_template("list.html", items = shopping_lists.get_items())
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
