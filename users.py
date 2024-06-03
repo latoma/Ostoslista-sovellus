428997e73e4f4114f03b3c2168dcd4b6f3839233
latoma <127959432+latoma@users.noreply.github.com>
2024-06-03 14:51:24 +0300

    login monitoring failure and fix

--- users.py
+++ users.py
@@ -5,6 +5,9 @@ from sqlalchemy.sql import text
 import shopping_lists, recipes
 import base64
 
+
+# SECURITY LOGGING AND MONITORING FAILURE:
+# No login threat detection
 def login(username, password):
     sql = text('SELECT user_id, password FROM users WHERE username=:username')
     result = db.session.execute(sql, {"username":username})
@@ -18,6 +21,26 @@ def login(username, password):
             return True
         else:
             return False
+        
+# FIX:
+# # Define a dictionary to store login attempts
+# login_attempts = {}
+
+# # Define constants for login attempt thresholds and lockout duration
+# MAX_LOGIN_ATTEMPTS = 5
+# LOCKOUT_DURATION_MINUTES = 15
+# ...
+# # Logic is something like this:
+#
+# # Check if the user is currently locked out due to previous failed attempts
+# if username in login_attempts and login_attempts[username]['locked']:
+#     if login_attempts[username]['lockout_time'] > datetime.now():
+#         # User is still locked out
+#         return False
+#     else:
+#         # Lockout period has expired, reset login attempts
+#         login_attempts[username]['attempts'] = 0
+#         login_attempts[username]['locked'] = False
 
 def logout():
     del session["user_id"]
