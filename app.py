0673399d2bfed9030e8cc9cdd7d8bab2cdc4f15a
latoma <127959432+latoma@users.noreply.github.com>
2024-06-05 14:08:47 +0300

    Secret key flaw + fix

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from os import getenv
 
 app = Flask(__name__, static_url_path='/static')
-app.secret_key = getenv("SECRET_KEY")
+app.secret_key = 'a32ca29848ff340a5c28845c62f4af6b'
+# FIX:
+# app.secret_key = getenv("SECRET_KEY")
 
 import routes
